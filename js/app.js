8a7db6b835642d83cb0be6e698ec8f5354c2e243
ajtheblueninja <ajdtheblueninja@gmail.com>
2020-08-08 12:37:22 -0500

    added content

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-'use strict';
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+var userName = prompt('Please Enter Your Name.')
 
+alert('hello' + userName + 'click ok to proceed');
 
+if(hourNow > 18) {
+    greeting = 'Good Evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good Morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h2>' + greeting + '</h2>');
 
 console.log(This file is connected and ready to return.);
---
+++ js/app2.js
@@ -0,0 +1,4 @@
+var hourNow = today.getHours
+var minuteNow = today.getMinutes
+
+document.write('<h1>' hourNow + ":" + minuteNow '</h1>');
