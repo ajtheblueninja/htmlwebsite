7de6af3985fcc11dda31f4026743d638c850dbad
ajtheblueninja <ajdtheblueninja@gmail.com>
2020-08-11 12:22:28 -0500

    updates

--- js/app.js
+++ js/app.js
@@ -7,6 +7,12 @@ var greeting;
 
 var userName = prompt('Please Enter Your Name.')
 
+var number1 = 5;
+
+var number2 = 6;
+
+var math = number1 + number2;
+
 if(hourNow > 18) {
     greeting = 'Good Evening!';
 } else if (hourNow > 12) {
@@ -17,6 +23,11 @@ if(hourNow > 18) {
     greeting = 'Welcome!';
 }
 
-document.getElementById('placeholder1').textContent = greeting + " " + userName;
+
+document.getElementById('greeting-placeholder').textContent = greeting + " " + userName;
+
+document.getElementById('personal-hello').textContent = "Hello " + userName + " this is my page it isn't much but I will continue to add to this site, thanks for coming to my page and have a nice day!";
+
+document.getElementById('math').textContent = "5 * 6 = " + math +" The website did this equation itself";
 
 alert('hello ' + userName + ' click ok to proceed');
